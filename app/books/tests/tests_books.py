4d49948f050cbe7cb8c57b2bf5577ae2151e4fdf
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2024-04-26 23:04:29 +0200

    added jwt

--- app/books/tests/tests_books.py
+++ app/books/tests/tests_books.py
@@ -9,6 +9,6 @@ class TestBookViewSet(APITestCase):
         self.author1 = Author.objects.create(name="Author 1", birth_date="1990-01-01")
         self.book1 = Book.objects.create(title="Book 1", author=self.author1)
         return super().setUp()
-    def test_list(self):
+    def test_books_list(self):
         response = self.client.get(reverse("books-list"))
         self.assertEqual(response.status_code, 200)
